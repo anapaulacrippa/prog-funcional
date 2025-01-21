3017243204582a3128f1d93bb7c73c46282215d3
Ana Paula <164682309+anapaulacrippa@users.noreply.github.com>
2025-01-20 21:30:41 -0300

    arruma comentarios

--- lista-5/ex-06.rkt
+++ lista-5/ex-06.rkt
@@ -8,20 +8,19 @@
 
 ;; Lista(Inteiro) -> Lista(Inteiro)
 ;;
-;; Cria uma nova lista removendo todos os valores nulos de uma lista de inteiros.
-
+;; Remove todos os valores nulos de uma lista de inteiros.
 
 (examples
  (check-equal? (remove-nulos empty) '())
  (check-equal? (remove-nulos (cons 1 (cons 2 (cons 3 (cons 4 empty))))) (list 1 2 3 4))
  (check-equal? (remove-nulos (list 0 0 0 0 0 0 0 0)) '())
  (check-equal? (remove-nulos (list 30 6 19 66 2 72 3 9 0 5 15 0 2 13)) (list 30 6 19 66 2 72 3 9 5 15 2 13))
- (check-equal? (remove-nulos (cons 13 (cons 10 (cons 3 (cons 20 (cons 0 empty)))))) (list 13 10 3 20)) )
+ (check-equal? (remove-nulos (cons 13 (cons 10 (cons 3 (cons 20 (cons 0 empty)))))) (list 13 10 3 20)))
 
 (define (remove-nulos lst)
   (cond
    [(empty? lst) '()]
-   [(not (equal? 0 (first lst))) ; se for diferente de 0, insere na lista
+   [(not (equal? 0 (first lst)))  ; se for diferente de 0, insere na lista
     (cons (first lst) (remove-nulos (rest lst)))]
-   [else ; se for igual a 0, deve ser ignorado (passa o resto da lista)
+   [else  ; se for igual a 0, deve ser ignorado 
     (remove-nulos (rest lst))]))
