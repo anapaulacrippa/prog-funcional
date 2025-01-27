22f77c867b4864915bb55157942734a6a4e7743c
Ana Paula <164682309+anapaulacrippa@users.noreply.github.com>
2025-01-26 21:32:24 -0300

    porao de energia criado

--- trab-2/trab-2.rkt
+++ trab-2/trab-2.rkt
@@ -161,22 +161,19 @@
      (navegar (ambiente-saida sala) jogador)]
     [else
      ; ainda há interações possíveis
-     (displayln "\nVocê vê os seguintes objetos:")
-     (for-each displayln (map objeto-nome (ambiente-objetos sala)))
+     (displayln "\nVocê vê os seguintes objetos:") (for-each displayln (map objeto-nome (ambiente-objetos sala)))
      (displayln "\nDigite o nome do objeto que deseja explorar")
-     (define obj (first (busca-objeto sala (read-line)))) ; acessa o objeto a ser explorado
-     ; interação do objeto escolhido e chamada para o enigma
-     (define jog-atual ((objeto-interacao obj) jogador sala))
-     ; com o enigma resolvido, remove-se o objeto do ambiente
-     (define ambiente-atual (struct-copy ambiente sala
-                                       [objetos (remove obj (ambiente-objetos sala))]))
-     (explorar ambiente-atual jog-atual)]
+     (define obj (first (busca-objeto sala (read-line))))  ; acessa o objeto a ser explorado
+     (displayln (objeto-descricao obj))
+     (define jog-atual ((objeto-interacao obj) jogador sala))  ; interação do objeto escolhido e chamada para o enigma
+     (explorar (struct-copy ambiente sala [objetos (remove obj (ambiente-objetos sala))]) ; com o enigma resolvido, remove-se o objeto do ambiente
+               jog-atual)]
   ))
 
 
 (define (executar-jogo)
   ; explicação inicial do funcionamento do jogo
-  (displayln "Seja bem-vindo ao Scape Room: Hacker vs. Hacker!") ; melhorar isso
+  (displayln "Seja bem-vindo ao Escape Room: Hacker vs. Hacker!") ; melhorar isso
 
   ; escolha do nome do jogador
   (displayln "Digite o nome do jogador: ")
@@ -205,9 +202,99 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
+;; (3) LABORATÓRIO DE CRIPTOGRAFIA
+
+;; Interação 1
+;; Objeto 1
+;; Enigma 1
+
+;; Interação 2
+;; Objeto 2
+;; Enigma 2
+
+;; Interação 3
+;; Objeto 3
+;; Enigma 3
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; (4) PORÃO DE ENERGIA
+
+;; Jogador Ambiente -> Enigma | Jogador
+;;
+;; Representa uma interação possível com o objeto "Cabos Soltos".
+;; Inicia o enigma "Reconectar cabos".
+
+(define (arrumar-cabos jogador ambiente)
+  (displayln "Reconectando cabos...")
+  (iniciar-enigma reconectar-cabos jogador))
+
+;; Objeto 1
+(define cabos-soltos (objeto "Cabos Soltos" "\nCabos espalhados pela sala, parcialmente desconectados. Eles fazem parte de um sistema mais complexo, devem ser ligados na sequência certa." 
+                             arrumar-cabos))
+
+;; Enigma 1
+(define reconectar-cabos (enigma "\nQuatro cabos coloridos (Azul, Amarelo, Vermelho, Verde) estão desconectados.\nVocê precisa conectá-los na sequência certa usando '->' para evitar uma sobrecarga."
+                                 "Vermelho -> Verde -> Azul -> Amarelo"
+                                 (list "1:\nSiga a lógica do semáforo, mas lembre-se de que Verde é o próximo passo."
+                                       "2:\nNão se apresse, a ordem certa é fundamental para evitar um curto-circuito."
+                                       "3:\nEntre o calor do vermelho e a calma do azul, o verde se destaca. Depois disso, o Sol brilha amarelo.")
+                                 "Os cabos estão conectados e o gerador pode ser ligado. A sequência de cabos correta foi adicionada ao seu inventário."
+                                 (list "Sequência de cabos correta: Vermelho -> Verde -> Azul -> Amarelo")))
+
+;; Jogador Ambiente -> Enigma | Jogador
+;;
+;; Representa uma interação possível com o objeto "Painel de Controle de Energia".
+;; Inicia o enigma "Restaurar Energia".
+
+(define (arrumar-energia jogador ambiente)
+  (displayln "Restaurando energia...")
+  (iniciar-enigma restaurar-energia jogador))
+
+;; Objeto 2
+(define painel-controle-energia (objeto "Painel de Controle de Energia" "\nUm painel de controle com diversos botões e luzes piscando. Para um sistema de energia instável, o painel é a chave para restaurar a energia." 
+                                        arrumar-energia))  ; interação
+
+;; Enigma 2
+(define restaurar-energia (enigma "\nO painel de controle está piscando com códigos de erro. Digite o código correto para reiniciar o sistema."
+                                  "2306"
+                                  (list "1:\nOs códigos de erro são uma pista para o que está faltando. Tente pensar em um pioneiro da computação."
+                                        "2:\nÉ alguém que fez uma contribuição significativa na decifração durante a guerra."
+                                        "3:\nO aniversário deste gênio pode ser a chave para o código.")
+                                  "Energia restaurada no sistema! O código para reiniciar o sistema foi adicionado ao seu inventário."
+                                  (list "Código para reiniciar o sistema: 2306")))
+
+
+;; Interação 3 - Ativar o gerador
+;; Interações possíveis com o objeto "Gerador Principal"
+(define (restaurar-gerador jogador ambiente)
+  (displayln "Restaurando e ativando gerador...")
+  (iniciar-enigma ativar-gerador jogador))
+
+;; Objeto 3
+(define gerador-principal (objeto "Gerador Principal" "O gerador garante o fornecimento de energia sempre que há falhas na corrente elétrica, assegurando a continuidade do sistema." 
+                                  restaurar-gerador))  ; interação
+
+;; Enigma 3
+(define ativar-gerador (enigma "\nO gerador está quase pronto para funcionar, mas precisa de um código especial para ser ativado. Encontre o código no painel e digite a sequência correta.\n\n--------- PAINEL ---------\n| 4 | 8 | 1 | 5 | 1 | 6 |\n| 2 | 3 | 4 | 2 | 7 | 9 |\n--------------------------\nO código que você precisa está escondido aqui. A sequência correta pode estar bem visível entre outros números."
+                               "4815162342"
+                               (list "1:\nO código está escondido em uma anotação no painel."
+                                     "2:\nVerifique se há algum padrão numérico que possa ser a chave."
+                                     "3:\nÀs vezes, a solução está bem à vista, entre outros números.")
+                               "Gerador ativado com sucesso! O sistema está em pleno funcionamento. O código do gerador foi adicionado ao seu inventário."
+                               (list "Código do gerador: 4815162342")))
+
+(define porao-energia (ambiente "Porão de Energia" "Local onde você restaura a energia (não a sua). Os geradores antigos e os paineis piscando mostram que há algo fora do lugar."
+                                   (list cabos-soltos ) ; objetos
+                                   (list reconectar-cabos ) ; enigmas
+                                   #f ; lab-cripto ; saída possível
+                                   ))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
 ;; (5) DATA CENTER
 
-;; Jogador Ambiente -> (list Jogador Ambiente)
+;; Jogador Ambiente -> Enigma | Jogador
 ;;
 ;; Representa uma interação possível com o objeto "Servidor Principal".
 ;; Inicia o enigma "Descriptografar Senha".
@@ -218,7 +305,7 @@
 
 ;; Objeto 1
 (define servidor-principal (objeto "Servidor Principal" "\nA peça central do sistema. Um terminal exibe informações críticas e opções avançadas de acesso." 
-                                   descobrir-senha)) ; interação
+                                   descobrir-senha))  ; interação
 
 ;; Enigma 1
 (define descriptografar-senha (enigma "\nA quantidade de bits em um byte padrão.\nO número máximo que pode ser representado em 8 bits sem sinal.\nO total de combinações possíveis com 2 entradas em uma tabela verdade.\nO total de níveis de cache na maioria das CPUs modernas"
@@ -229,19 +316,18 @@
                                       "Decodificação bem sucedida!"
                                       (list "Senha do servidor")))
 
-;; Jogador Ambiente -> (list Jogador Ambiente)
+;; Jogador Ambiente -> Enigma | Jogador
 ;;
 ;; Representa uma interação possível com o objeto "Estação de Trabalho".
 ;; Inicia o enigma "Identificar Padrões Anômalos".
 
 (define (descobrir-anomalos jogador ambiente)
-  (displayln "\nDescriptografando senha...")
-  (iniciar-enigma identificar-anomalo jogador))
+  (displayln "\nDescriptografando senha...") (iniciar-enigma identificar-anomalo jogador))
 
 ;; Objeto 2
 (define estacao-trabalho (objeto "Estação de Trabalho" 
                                  "\nUma estação com múltiplos monitores exibindo gráficos, logs de rede e alertas de segurança." 
-                                 descobrir-anomalos)) ; interação
+                                 descobrir-anomalos))  ; interação
 
 ;; Enigma 2
 (define identificar-anomalo (enigma "\nOs logs da estação mostram um padrão incomum de acessos a um servidor crítico. A cada 5 segundos, um IP desconhecido está tentando acessar o sistema.\nSeu desafio é identificar o IP anômalo e bloquear a ameaça antes que seja tarde demais.\n\nAcessos nos últimos segundos:\n192.168.0.101\n192.168.0.102\n192.168.0.103\n10.0.0.255\n192.168.0.101\n192.168.0.102\n192.168.0.103\n10.0.0.255\n192.168.0.101\n192.168.0.102\n192.168.0.103\n10.0.0.255"
@@ -255,9 +341,8 @@
 (define data-center (ambiente "Data Center" "\nAqui fica o servidor principal do sistema e você pode coletar dados críticos"
                               (list servidor-principal estacao-trabalho)  ; objetos
                               (list descriptografar-senha)  ; enigmas
-                              porao-energia  ; saída possível
-                              )) 
-
+                              porao-energia))  ; saída possível
+                              
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; (6) SALA DE CONTROLE
@@ -272,7 +357,7 @@
 
 ;; Objeto 1
 (define teclado-desgastado (objeto "Teclado Desgastado" "Um teclado velho e usado, com algumas teclas apagadas. Pode ser útil para inserir comandos."
-                                   examinar-simbolos)) ; interação
+                                   examinar-simbolos))  ; interação
 
 ;; Enigma 1
 (define arquivo-criptografado (enigma "\nCada letra carrega o peso de algo repetido infinitas vezes...\n\n‘     1    2   3    4   5   6   7   8   9   0  -  =  backspace\ntab   _   W   E   _  T   Y   U   I   O   P   ́  [   enter\ncapslock  A  S  D   _   _    H   _   K   _  Ç  ~   ]\nshift \\   Z   X   C   V   B   N   M   ,   .   /  shift\nctrl   fn   alt          space         ctrl    /"
@@ -283,17 +368,16 @@
                                       "Inserindo código de acesso...\n...\nAcesso ao arquivo desbloqueado!\nA senha foi adicionada ao seu inventário."
                                       (list "Senha do arquivo")))
 
-;; Jogador Ambiente -> (list Jogador Ambiente)
+;; Jogador Ambiente -> Enigma | Jogador
 ;;
 ;; Representa uma interação possível com o objeto "Painel de Controle".
 ;; Inicia o enigma "Quebrar Senha".
 
-(define (inserir-cod-acesso jogador ambiente)
-   (iniciar-enigma quebrar-senha jogador))
+(define (inserir-cod-acesso jogador ambiente) (iniciar-enigma quebrar-senha jogador))
 
 ;; Objeto 2
 (define painel-controle (objeto "Painel de Controle" "Um painel com diversas luzes e botões. Ele monitora e gerencia o acesso ao sistema." 
-                                inserir-cod-acesso))
+                                inserir-cod-acesso))  ; interação
 
 ;; Enigma 2
 (define quebrar-senha (enigma "\nEsse arquivo contém o endereço para um local conhecido, mas inseguro…\nsggk://wrm.fvn.yi"
@@ -304,18 +388,17 @@
                               "O endereço do site (seguro) do DIN foi adicionado ao seu inventário. Seja cauteloso! :O"
                               (list "http://din.uem.br")))
 
-;; Jogador Ambiente -> (list Jogador Ambiente)
+;; Jogador Ambiente -> Enigma | Jogador
 ;;
 ;; Representa uma interação possível com o objeto "Monitor de Rede".
 ;; Inicia o enigma "Explorar Vulnerabilidades".
 
 (define (brecha-sistema jogador ambiente)
-  (displayln "\nExplorando vulnerabilidades...")
-  (iniciar-enigma explorar-vulnerabilidades jogador))
+  (displayln "\nExplorando vulnerabilidades...") (iniciar-enigma explorar-vulnerabilidades jogador))
 
 ;; Objeto 3
-(define monitor-rede (objeto "Monitor de Rede" "Um monitor que exibe conexões de rede e informações em tempo real." 
-                             brecha-sistema))
+(define monitor-rede (objeto "Monitor de Rede" "Um monitor que exibe conexões de rede e informações em tempo real."
+                             brecha-sistema))  ; interação
 
 ;; Enigma 3
 (define explorar-vulnerabilidades (enigma "\nNão seguro! http://din.uem.br\nSua conexão com esse site não é segura. É recomendado não fornecer informações confidenciais a esse site (por exemplo, senhas ou cartões de crédito),\nporque elas podem ser roubadas por invasores."
@@ -326,55 +409,7 @@
                                          "\nAcesso ao Data Center desbloqueado!\nUma chave foi adicionada ao seu inventário."
                                          (list "Chave Data Center")))
 
-
-(define sala-controle (ambiente "Sala de Controle" "\nO ponto de partida, onde você fará a análises e monitoramentos do sistema."
-                                (list teclado-desgastado painel-controle monitor-rede) ; objetos
-                                (list arquivo-criptografado quebrar-senha explorar-vulnerabilidades) ; enigmas
-                                data-center ; saídas disponíveis
-                                ))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; (3) PORÃO DE ENERGIA
-
-(define porao-energia (ambiente "Porão de Energia" "Descrição do Porão de Energia"
-                                   (list ) ; objetos
-                                   (list ) ; enigmas
-                                   #f ; lab-cripto 
-                                   ))
-
-;; Interação 1
-;; Objeto 1
-;; Enigma 1
-
-;; Interação 2
-;; Objeto 2
-;; Enigma 2
-
-;; Interação 3
-;; Objeto 3
-;; Enigma 3
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; (4) LABORATÓRIO DE CRIPTOGRAFIA
-
-
-
-
-;; Interação 1
-;; Objeto 1
-;; Enigma 1
-
-;; Interação 2
-;; Objeto 2
-;; Enigma 2
-
-;; Interação 3
-;; Objeto 3
-;; Enigma 3
-
-
-
-
-(define player (jogador "Ana" '() 10 sala-controle))
+(define sala-controle (ambiente "Sala de Controle" "\nO ponto de partida, onde você fará análises e monitoramentos do sistema."
+                                (list teclado-desgastado painel-controle monitor-rede)  ; objetos
+                                (list arquivo-criptografado quebrar-senha explorar-vulnerabilidades)  ; enigmas
+                                data-center))  ; saída disponível                                
